437bfd32499c78fbe24a54896774cbd3a61219ac
kmotlhale <keoagile@byteage.co.za>
2024-04-12 21:17:37 +0200

    initial commit

--- Program.cs
+++ Program.cs
@@ -1,9 +1,27 @@
-﻿namespace sfgticketassignment;
+var builder = WebApplication.CreateBuilder(args);
 
-class Program
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hell000, World!");
-    }
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
